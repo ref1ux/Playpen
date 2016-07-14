adbd59f1b7d1ac327e6bcc20c4ad203af6eaff61
Wes <ref1ux@users.noreply.github.com>
2016-07-14 11:25:28 +1000

    Added Hello, World! Java starter
    Starting with Java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+/*
+* First Java program, which says "Hello, world!"
+*/
+public class Hello {  // Save as "Hello.java"
+	public static void main(String[] args) {  // program entry point
+		System.out.println("Hello, world!"); // print message
+	}
+}	
